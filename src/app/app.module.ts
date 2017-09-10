5ae765908913d76bedb16e1af771f644b6d19454
cicithesquirrel <cyrilc_mail-github@yahoo.fr>
2017-09-10 15:04:35 +0200

    Added: Module for "movies' list"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoviesListModule } from './movies-list/movies-list.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MoviesListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
