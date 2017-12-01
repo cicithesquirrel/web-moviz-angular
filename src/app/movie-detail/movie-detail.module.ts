dc40b8ab600b11cdc661f94d6ec18ecaa2a6bb1d
cchene <cchene@e-citiz.com>
2017-12-01 11:13:18 +0100

    Added links from detail

--- src/app/movie-detail/movie-detail.module.ts
+++ src/app/movie-detail/movie-detail.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   declarations: [MainComponent],
   bootstrap: [MainComponent],
