106ecccd300d1dd0c361ee76bb86cbd649fa0d74
cchene <cchene@e-citiz.com>
2017-12-01 09:58:46 +0100

    Added link to detail

--- src/app/movies-list/movies-list.module.ts
+++ src/app/movies-list/movies-list.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ListComponent } from './list/list.component';
 import { ItemComponent } from './item/item.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ],
   declarations: [ListComponent, ItemComponent],
   bootstrap: [ListComponent],
